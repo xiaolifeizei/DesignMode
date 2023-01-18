76506ffe6008dcbfd07bf8ecb21d213db57266ec
xiaolifeizei <xiaolifeizei@163.com>
2023-01-18 16:36:47 +0800

    完善

---
+++ AbstractFactory/src/main/java/com/matrix/abstractfactory/SubjectCategoryAFactory.java
@@ -0,0 +1,32 @@
+package com.matrix.abstractfactory;
+
+/**
+ * @author : cui_feng
+ * @since : 2023-01-06 10:35
+ */
+public class SubjectCategoryAFactory implements SubjectFactory {
+
+    @Override
+    public SubjectFirst getSubjectFirst(String type) {
+        if ("one".equals(type)) {
+            return new SubjectFirstOneImpl();
+        }
+
+        if ("two".equals(type)) {
+            return new SubjectFirstTwoImpl();
+        }
+        return null;
+    }
+
+    @Override
+    public SubjectSecond getSubjectSecond(String type) {
+        if ("one".equals(type)) {
+            return new SubjectSecondOneImpl();
+        }
+
+        if ("two".equals(type)) {
+            return new SubjectSecondTwoImpl();
+        }
+        return null;
+    }
+}
